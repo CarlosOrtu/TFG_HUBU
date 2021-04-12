5bf6834b6ae32e77c0a345f84c0d3562a14c525c
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-12 11:34:29 +0200

    Añadir test datos personales #46

---
+++ Laravel/tests/Feature/ModificarContrasenaTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Usuarios;
+use Tests\TestCase;
+
+class ModificarContrasenaTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $usuarioAModificar = new Usuarios();
+        $usuarioAModificar->id_usuario = 999;
+        $usuarioAModificar->nombre = "Usuario";
+        $usuarioAModificar->apellidos = "Apellido1 Apellido2";
+        $usuarioAModificar->email = "usuario@gmail.com";
+        $usuarioAModificar->contrasena = bcrypt("1234");
+        $usuarioAModificar->id_rol = 2;      
+        $usuarioAModificar->save();  
+        //Realizamos el login para poder acceder a todos las rutas de la web
+        $response = $this->get('/login')->assertSee('Login');
+        $credentials = [
+            "email" => "usuario@gmail.com",
+            "password" => "1234",
+        ];
+        $response = $this->post('/login', $credentials);
+    }
+
+    protected function tearDown(): void
+    {
+        //Eliminamos el usuario
+        Usuarios::find(999)->delete();
+        parent::tearDown();
+    }
+
+    /** @test */
+    //Caso de prueba 1
+    public function modificarContrasenaCorrectoTest()
+    {
+        //Accedemos la vista datospersonales 
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "1234",
+            "contrasena_nueva" => "1111",
+            "contrasena_nueva_repetida" => "1111",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobmos que el usuario este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1111", $usuario->contrasena));
+    }
+
+    /** @test */
+    //Caso de prueba 2
+    public function modificarContrasenaContrasenaAntiguaVaciaTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "",
+            "contrasena_nueva" => "1111",
+            "contrasena_nueva_repetida" => "1111",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('contrasena_antigua');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/modificar/contrasena');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1234", $usuario->contrasena));
+    }
+
+    /** @test */
+    //Caso de prueba 3
+    public function modificarContrasenaContrasenaNuevaVaciaTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "1234",
+            "contrasena_nueva" => "",
+            "contrasena_nueva_repetida" => "1111",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('contrasena_nueva');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/modificar/contrasena');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1234", $usuario->contrasena));
+    }
+
+    /** @test */
+    //Caso de prueba 4
+    public function modificarContrasenaContrasenaNuevaRepetidaVaciaTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "1234",
+            "contrasena_nueva" => "1111",
+            "contrasena_nueva_repetida" => "",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('contrasena_nueva_repetida');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/modificar/contrasena');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1234", $usuario->contrasena));
+    }
+
+    /** @test */
+    //Caso de prueba 2
+    public function modificarContrasenaContrasenaAntiguaErroneaTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "2222",
+            "contrasena_nueva" => "1111",
+            "contrasena_nueva_repetida" => "1111",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('contrasena_antigua');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/modificar/contrasena');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1234", $usuario->contrasena));
+    }
+
+    /** @test */
+    //Caso de prueba 2
+    public function modificarContrasenaContrasenasNuevasNoCoincidentesTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/modificar/contrasena')->assertSee('Modificar contraseña');
+        $contrasena = [
+            "contrasena_antigua" => "1234",
+            "contrasena_nueva" => "2222",
+            "contrasena_nueva_repetida" => "1111",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/modificar/contrasena', $contrasena);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('contrasena_nueva');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/modificar/contrasena');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::find(999);
+        $this->assertTrue(password_verify("1234", $usuario->contrasena));
+    }
+}
---
+++ Laravel/tests/Feature/ModificarDatosPersonalesTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Usuarios;
+
+class ModificarDatosPersonalesTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $usuarioAModificar = new Usuarios();
+        $usuarioAModificar->id_usuario = 999;
+        $usuarioAModificar->nombre = "Usuario";
+        $usuarioAModificar->apellidos = "Apellido1 Apellido2";
+        $usuarioAModificar->email = "usuario@gmail.com";
+        $usuarioAModificar->contrasena = bcrypt("1234");
+        $usuarioAModificar->id_rol = 2;      
+        $usuarioAModificar->save();  
+        //Realizamos el login para poder acceder a todos las rutas de la web
+        $response = $this->get('/login')->assertSee('Login');
+        $credentials = [
+            "email" => "usuario@gmail.com",
+            "password" => "1234",
+        ];
+        $response = $this->post('/login', $credentials);
+    }
+
+    protected function tearDown(): void
+    {
+        //Eliminamos el usuario
+        Usuarios::find(999)->delete();
+        parent::tearDown();
+    }
+
+    /** @test */
+    //Caso de prueba 1
+    public function modificarDatosCorrectoTest()
+    {
+        //Accedemos la vista datospersonales 
+        $response = $this->get('/datos/personales')->assertSee('Modificar datos personales');
+        $usuario = [
+            "nombre" => "UsuarioModificado",
+            "apellidos" => "ApellidoModificado Apellido2",
+            "correo" => "usuariomodificado@gmail.com",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/datos/personales', $usuario);
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobamos que en la vista datospersonales se vea el nuevo usuario
+        /*
+        $view = $this->view('datospersonales');
+        $view->assertSee('UsuarioModificado');
+        $view->assertSee('ApellidoModificado Apellido2');
+        $view->assertSee('usuariomodificado@gmail.com');
+        */
+        //Comprobmos que el usuario este incluido en la base de datos
+        $usuario = Usuarios::where('nombre','UsuarioModificado')->first();
+        $this->assertTrue(!empty($usuario));
+        $this->assertTrue($usuario->apellidos == "ApellidoModificado Apellido2");
+        $this->assertTrue($usuario->email == "usuariomodificado@gmail.com");
+    }
+
+    /** @test */
+    //Caso de prueba 2
+    public function modificarDatosNombreVacioTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/datos/personales')->assertSee('Modificar datos personales');
+        $usuario = [
+            "nombre" => "",
+            "apellidos" => "ApellidoModificado Apellido2",
+            "correo" => "usuariomodificado@gmail.com",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/datos/personales', $usuario);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('nombre');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::where('nombre','UsuarioModificado')->first();
+        $this->assertTrue(empty($usuario));
+    }
+
+    /** @test */
+    //Caso de prueba 3
+    public function modificarDatosApellidoVacioTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/datos/personales')->assertSee('Modificar datos personales');
+        $usuario = [
+            "nombre" => "UsuarioModificado",
+            "apellidos" => "",
+            "correo" => "usuariomodificado@gmail.com",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/datos/personales', $usuario);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('apellidos');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::where('nombre','UsuarioModificado')->first();
+        $this->assertTrue(empty($usuario));
+    }
+
+    /** @test */
+    //Caso de prueba 4
+    public function modificarDatosCorreoVacioTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/datos/personales')->assertSee('Modificar datos personales');
+        $usuario = [
+            "nombre" => "UsuarioModificado",
+            "apellidos" => "ApellidoModificado Apellido2",
+            "correo" => "",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/datos/personales', $usuario);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('correo');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::where('nombre','UsuarioModificado')->first();
+        $this->assertTrue(empty($usuario));
+    }
+
+    /** @test */
+    //Caso de prueba 5
+    public function modificarDatosCorreoNoValidoVacioTest()
+    {
+        //Accedemos la vista datospersonales
+        $response = $this->get('/datos/personales')->assertSee('Modificar datos personales');
+        $usuario = [
+            "nombre" => "UsuarioModificado",
+            "apellidos" => "ApellidoModificado Apellido2",
+            "correo" => "usuariomodificadogmail.com",
+        ];
+        //Realizamos la solicitud post con los datos del usuario definidos anteriormente
+        $response = $this->put('/datos/personales', $usuario);
+        //Comprobamos que devuelve error en el campo nombre
+        $response->assertSessionHasErrors('correo');
+        //Comprobamos si se redirige correctamente
+        $response->assertRedirect('/datos/personales');
+        //Comprobmos que el usuario no este incluido en la base de datos
+        $usuario = Usuarios::where('nombre','UsuarioModificado')->first();
+        $this->assertTrue(empty($usuario));
+    }
+}
