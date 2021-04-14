4c5b8e19db44fb1bedcbba1c172057508b106b48
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-14 13:14:00 +0200

    Modificar sidebar #50

--- Laravel/resources/views/layouts/sidebarenfermedad.blade.php
+++ Laravel/resources/views/layouts/sidebarenfermedad.blade.php
@@ -3,97 +3,112 @@
     <div class="border rounded p-3 bg-transparent">
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton1" data-bs-toggle="collapse" data-bs-target="#menu1" aria-expanded="false">
-          Opcion 1
+          Paciente
         </button>
         <div class="collapse" id="menu1">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="text-white rounded">Opcion 1.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 1.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 1.3</a></li>
+            <li><a href="{{ route('datospaciente', $paciente->id_paciente) }}" class="text-white rounded">Paciente</a></li>
           </ul>
         </div>
       </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton2" data-bs-toggle="collapse" data-bs-target="#menu2" aria-expanded="false">
-          Opcion 2
+          Enfermedad
         </button>
         <div class="collapse" id="menu2">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="text-white rounded">Opcion 2.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 2.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 2.3</a></li>
+            <li><a href="#" class="text-white rounded">Sintomas</a></li>
+            <li><a href="#" class="text-white rounded">Metastasis</a></li>
+            <li><a href="#" class="text-white rounded">Biomarcadores</a></li>
+            <li><a href="#" class="text-white rounded">Pruebas realizadas</a></li>
+            <li><a href="#" class="text-white rounded">Tecnicas realizadas</a></li>
+            <li><a href="#" class="text-white rounded">Otros tumores</a></li>
           </ul>
         </div>
       </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton3" data-bs-toggle="collapse" data-bs-target="#menu3" aria-expanded="false">
-          Opcion 3
+          Antecedentes
         </button>
         <div class="collapse" id="menu3">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
+            <li><a href="#" class="rounded text-white">Antecedentes médicos</a></li>
+            <li><a href="#" class="text-white rounded">Antecedentes oncológicos</a></li>
+            <li><a href="#" class="text-white rounded">Antecedentes familiares</a></li>
           </ul>
         </div>
       </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton4" data-bs-toggle="collapse" data-bs-target="#menu4" aria-expanded="false">
-          Opcion 4
+          Tratamientos
         </button>
         <div class="collapse" id="menu4">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
+            <li><a href="#" class="rounded text-white">Quimioterapia</a></li>
+            <li><a href="#" class="text-white rounded">Radioterapia</a></li>
+            <li><a href="#" class="text-white rounded">Cirguia</a></li>
+            <li><a href="#" class="text-white rounded">Secuencia tratamientos</a></li>
           </ul>
         </div>
       </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton5" data-bs-toggle="collapse" data-bs-target="#menu5" aria-expanded="false">
-          Opcion 5
+          Reevaluaciones
         </button>
         <div class="collapse" id="menu5">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
+            <?php
+                $i = 1;
+            ?>
+            @foreach ($paciente->Reevaluaciones as $reevaluacion)
+            <li><a href="#" class="rounded text-white">Reevaluación {{ $i }}</a></li>
+            <?php
+                $i = $i + 1;
+            ?>
+            @endforeach
+            <li><a href="#" class="rounded text-white">Nueva reevaluación</a></li>
           </ul>
         </div>
       </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton6" data-bs-toggle="collapse" data-bs-target="#menu6" aria-expanded="false">
-          Opcion 6
+          Seguimientos
         </button>
         <div class="collapse" id="menu6">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
+            <?php
+                $i = 1;
+            ?>
+            @foreach ($paciente->Seguimientos as $seguimiento)
+            <li><a href="#" class="rounded text-white">Seguimiento {{ $i }}</a></li>
+            <?php
+                $i = $i + 1;
+            ?>
+            @endforeach
+            <li><a href="#" class="rounded text-white">Nuevo seguimiento</a></li>
           </ul>
         </div>
       </li>
-      <li class="mb-1">
-        <button class="btn btn-toggle text-white desplegable" id="boton7" data-bs-toggle="collapse" data-bs-target="#menu7" aria-expanded="false">
-          Opcion 7
-        </button>
-        <div class="collapse" id="menu7">
-          <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
-          </ul>
-        </div>
-      </li>
+      <div class="dropdown-divider"></div>
       <li class="mb-1">
         <button class="btn btn-toggle text-white desplegable" id="boton8" data-bs-toggle="collapse" data-bs-target="#menu8" aria-expanded="false">
-          Opcion 8
+          Comentario
         </button>
         <div class="collapse" id="menu8">
           <ul class="btn-toggle-nav list-unstyled ml-5 pb-1 small">
-            <li><a href="#" class="rounded text-white">Opcion 3.1</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.2</a></li>
-            <li><a href="#" class="text-white rounded">Opcion 3.3</a></li>
+            <?php
+                $i = 1;
+            ?>
+            <li><a href="#" class="rounded text-white">Comentario {{ $i }}</a></li>
+            <?php
+                $i = $i + 1;
+            ?>
+            <li><a href="#" class="rounded text-white">Nuevo comentario</a></li>
           </ul>
         </div>
       </li>
