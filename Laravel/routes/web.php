08f0b258bb8d20d7a0fe51b7531199e49fee4f28
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-18 03:29:59 +0200

    Crear rutas enfermedad #47

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PacientesController;
 use App\Http\Controllers\AdministradorController;
 use App\Http\Controllers\DatosPersonalesController;
 use App\Http\Controllers\DatosPacienteController;
+use App\Http\Controllers\EnfermedadController;
 
 
 //Rutas de login y logout
@@ -35,9 +36,39 @@ Route::post('/nuevo/usuario', [AdministradorController::class, 'crearNuevoUsuari
 Route::get('/modificar/usuario/{id}', [AdministradorController::class, 'verModificarUsuario'])->name('modificarusuario');
 Route::put('/modificar/usuario/{id}', [AdministradorController::class, 'modificarUsuario']);
 
-//Rutas de visualización de datos de un paciente
+//Rutas de visualización y modificación de datos de un paciente
 Route::get('/paciente/{id}', [DatosPacienteController::class , 'verPaciente'])->name('datospaciente');
 Route::put('/paciente/{id}', [DatosPacienteController::class , 'cambiarDatosPaciente']);
-
-
+//Rutas de visualización y modificación de datos de la enfermedad 
+Route::get('/paciente/{id}/enfermedad/datosgenerales', [EnfermedadController::class , 'verDatosEnfermedad'])->name('datosenfermedad');
+Route::put('/paciente/{id}/enfermedad/datosgenerales', [EnfermedadController::class , 'guardarDatosEnfermedad']);
+//Rutas de visualización, modificación y eliminación de sintomas
+Route::get('/paciente/{id}/enfermedad/sintomas', [EnfermedadController::class, 'verDatosSintomas'])->name('datossintomas');
+Route::post('/paciente/{id}/enfermedad/sintomas', [EnfermedadController::class, 'crearDatosSintomas'])->name('datossintomascrear');
+Route::put('/paciente/{id}/enfermedad/sintomas/{num_sintoma}', [EnfermedadController::class, 'modificarDatosSintomas'])->name('datossintomasmodificar');
+Route::delete('/paciente/{id}/enfermedad/sintomas/{num_sintoma}', [EnfermedadController::class, 'eliminarSintoma'])->name('datossintomaseliminar');
+//Rutas de visualización, creacción, modificación y eliminación de metastasis
+Route::get('/paciente/{id}/enfermedad/metastasis', [EnfermedadController::class, 'verMetastasis'])->name('metastasis');
+Route::post('/paciente/{id}/enfermedad/metastasis', [EnfermedadController::class, 'crearMetastasis'])->name('metastasiscrear');
+Route::put('/paciente/{id}/enfermedad/metastasis/{num_metastasis}', [EnfermedadController::class, 'modificarMetastasis'])->name('metastasismodificar');
+Route::delete('/paciente/{id}/enfermedad/metastasis/{num_metastasis}', [EnfermedadController::class, 'eliminarMetastasis'])->name('metastasiseliminar');
+//Rutas de visualización, creacción, modificación y eliminación de pruebas realizadas
+Route::get('/paciente/{id}/enfermedad/pruebas', [EnfermedadController::class, 'verPruebas'])->name('pruebas');
+Route::post('/paciente/{id}/enfermedad/pruebas', [EnfermedadController::class, 'crearPruebas'])->name('pruebascrear');
+Route::put('/paciente/{id}/enfermedad/pruebas/{num_prueba}', [EnfermedadController::class, 'modificarPruebas'])->name('pruebasmodificar');
+Route::delete('/paciente/{id}/enfermedad/pruebas/{num_prueba}', [EnfermedadController::class, 'eliminarPruebas'])->name('pruebaseliminar');
+//Rutas de visualización, creacción, modificación y eliminación de técnicas realizadas
+Route::get('/paciente/{id}/enfermedad/tecnicas', [EnfermedadController::class, 'verTecnicas'])->name('tecnicas');
+Route::post('/paciente/{id}/enfermedad/tecnicas', [EnfermedadController::class, 'crearTecnicas'])->name('tecnicascrear');
+Route::put('/paciente/{id}/enfermedad/tecnicas/{num_tecnica}', [EnfermedadController::class, 'modificarTecnicas'])->name('tecnicasmodificar');
+Route::delete('/paciente/{id}/enfermedad/tecnicas/{num_tecnica}', [EnfermedadController::class, 'eliminarTecnicas'])->name('tecnicaseliminar');
+//Rutas de visualización, creacción, modificación y eliminación de otros tumores
+Route::get('/paciente/{id}/enfermedad/otrostumores', [EnfermedadController::class, 'verOtrosTumores'])->name('otrostumores');
+Route::post('/paciente/{id}/enfermedad/otrostumores', [EnfermedadController::class, 'crearOtrosTumores'])->name('otrostumorescrear');
+Route::put('/paciente/{id}/enfermedad/otrostumores/{num_otrostumores}', [EnfermedadController::class, 'modificarOtrosTumores'])->name('otrostumoresmodificar');
+Route::delete('/paciente/{id}/enfermedad/otrostumores/{num_otrostumores}', [EnfermedadController::class, 'eliminarOtrosTumores'])->name('otrostumoreseliminar');
+//Rutas de visualización, creacción, modificación y eliminación de biomarcadores
+Route::get('/paciente/{id}/enfermedad/biomarcadores', [EnfermedadController::class, 'verBiomarcadores'])->name('biomarcadores');
+Route::post('/paciente/{id}/enfermedad/biomarcadores', [EnfermedadController::class, 'guardarBiomarcadores']);
+Route::delete('/paciente/{id}/enfermedad/biomarcadores/{num_biomarcador}', [EnfermedadController::class, 'eliminarbiomarcadores'])->name('eliminarbiomarcador');
 
