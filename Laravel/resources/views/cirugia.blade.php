db4efa7151ca928f6854cf44640775421b2042c4
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-20 13:46:44 +0200

    Crear vista cirugía #56

---
+++ Laravel/resources/views/cirugia.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-between mb-4">
+    <h6 class="align-self-end text-white">Paciente: {{ $paciente->nombre }}</h6>
+    <h1 class="align-self-center text-white panel-title">Cirugía</h1>
+    <h6 class="align-self-end text-white">Ultima modificación: {{ $paciente->ultima_modificacion }}</h6>
+</div>
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('fecha')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+<?php
+    $i = 1;
+?>
+@foreach ($paciente->Tratamientos->where('tipo','Cirugia') as $tratamiento)
+<form action="{{ route('cirugiamodificar', ['id' => $paciente->id_paciente, 'num_cirugia' => $i]) }}" method="post">
+  @CSRF
+  @method('put')
+  <h4 class="text-white panel-title">Cirugía {{ $i }}</h4>
+  <div class="my-4 input-group">
+    <div class="input-group-prepend">
+        <span class="input-group-text">Localización</span>
+    </div>
+    <select name="tipo" class="tipo form-control">
+      <option {{ $tratamiento->subtipo == 'Neumonectomía' ? 'selected' : '' }}>Neumonectomía</option>
+      <option {{ $tratamiento->subtipo == 'Lobectomía' ? 'selected' : '' }}>Lobectomía</option>
+      <option {{ $tratamiento->subtipo == 'Bilobectomía' ? 'selected' : '' }}>Bilobectomía</option>
+      <option {{ $tratamiento->subtipo == 'Segmentectomía' ? 'selected' : '' }}>Segmentectomía</option>
+      <option {{ $tratamiento->subtipo == 'Resección' ? 'selected' : '' }}>Resección</option>
+      <option {{ $tratamiento->subtipo == 'Resección atípica' ? 'selected' : '' }}>Resección atípica</option>
+    </select>
+  </div>
+  <div class="my-4 input-group">
+    <div class="input-group-prepend">
+        <span class="input-group-text">Fecha</span>
+    </div>
+    <input value="{{ $tratamiento->fecha_inicio }}" name="fecha" type="date" class="form-control" autocomplete="off">
+  </div>
+    <div class="d-flex justify-content-center">
+      <button type="submit" class="btn btn-primary">Modificar</button>
+</form>
+      <form action="{{ route('cirugiaeliminar', ['id' => $paciente->id_paciente, 'num_cirugia' => $i]) }}" method="post">
+        @CSRF
+        @method('delete')
+        <button class="ml-2 btn btn-warning">Eliminar</button>
+      </form>
+    </div>
+<?php
+  $i = $i + 1;
+?>
+@endforeach 
+<div class="mb-4 d-flex justify-content-strat">
+    <button id="boton_nuevocampo" class="btn btn-primary">Nueva cirugía</button>
+</div>
+<form id="nuevocampo" class="oculto" action="{{ route('cirugiacrear', ['id' => $paciente->id_paciente]) }}" method="post">
+  @CSRF
+  <div class="my-4 input-group">
+    <div class="input-group-prepend">
+        <span class="input-group-text">Tipo</span>
+    </div>
+    <select name="tipo" class="tipo form-control">
+      <option>Neumonectomía</option>
+      <option>Lobectomía</option>
+      <option>Bilobectomía</option>
+      <option>Segmentectomía</option>
+      <option>Resección</option>
+      <option>Resección atípica</option>
+    </select>
+  </div>
+  <div class="my-4 input-group">
+    <div class="input-group-prepend">
+        <span class="input-group-text">Fecha</span>
+    </div>
+    <input name="fecha" type="date" class="form-control" autocomplete="off">
+  </div>
+  <div class="d-flex justify-content-center mb-4">
+    <button type="submit" class="btn btn-primary">Guardar</button>
+  </div>
+</form>
+<script src="{{ asset('/js/nuevocampo.js') }}" type="text/javascript"></script>
+@endsection
