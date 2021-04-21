0421748872308d69da0477e6d2397b5b7bd43adf
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-22 00:56:17 +0200

    Crear modelo comentarios #67

---
+++ Laravel/app/Models/Comentarios.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentarios extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+	protected $table = 'comentarios';	
+	protected $primaryKey = 'id_comentario';
+
+}
