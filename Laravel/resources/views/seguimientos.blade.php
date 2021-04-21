6d1f0e4b3111b59155c6090567461c06bc2b5cf0
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-04-21 18:37:50 +0200

    Crear vistas seguimientos #62

---
+++ Laravel/resources/views/seguimientos.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+ 
+@section('content')
+<div class="d-flex justify-content-between mb-4">
+    <h6 class="align-self-end text-white">Paciente: {{ $paciente->nombre }}</h6>
+    <h1 class="align-self-center text-white panel-title">Seguimiento {{ $posicion }}</h1>
+    <h6 class="align-self-end text-white">Ultima modificación: {{ $paciente->ultima_modificacion }}</h6>
+</div>
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('fecha')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('fecha_fallecimiento')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('motivo_especificar')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@if ($message = Session::get('SQLerror'))
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+<form action="{{ route('modificarseguimiento', ['id' => $paciente->id_paciente, 'num_seguimiento' => $posicion]) }}" method="post">
+    @CSRF
+    @method('put')
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Fecha <br>seguimiento</span>
+      </div>
+      <input value="{{ $seguimiento->fecha }}" name="fecha" type="date" class="form-control" autocomplete="off">
+    </div>
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Estado actual <br> del paciente</span>
+      </div>
+      <select name="estado" class="tipoNuevo form-control">
+        <option {{ $seguimiento->estado == 'Vivo sin enfermedad' ? 'selected' : '' }}>Vivo sin enfermedad</option>
+        <option {{ $seguimiento->estado == 'Vivo con enfermedad' ? 'selected' : '' }}>Vivo con enfermedad</option>
+        <option {{ $seguimiento->estado == 'Fallecido' ? 'selected' : '' }}>Fallecido</option>
+      </select>
+    </div>
+    <div class="oculto ml-2 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Motivo del <br>fallecimiento</span>
+      </div>
+      <select name="motivo" class="tipo form-control">
+        <option {{ $seguimiento->fallecido_motivo == 'Fallecido' ? 'selected' : '' }}>Enfermedad</option>
+        <option {{ preg_match("/^Otro: /", $seguimiento->fallecido_motivo) ? 'selected' : '' }}>Otro</option>
+      </select>    
+    </div>
+    <div class="oculto ml-4 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Especificar <br>fallecimiento</span>
+      </div>
+      @if(preg_match("/^Otro: /", $seguimiento->fallecido_motivo))
+      <input value="{{ substr($seguimiento->fallecido_motivo, 6) }}" name="motivo_especificar" class="form-control" autocomplete="off">
+      @else
+      <input name="motivo_especificar" class="form-control" autocomplete="off">
+      @endif
+    </div>
+    <div class="oculto ml-2 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Fecha del <br> fallecimiento</span>
+      </div>
+      <input value="{{ $seguimiento->fecha_fallecimiento }}" name="fecha_fallecimiento" type="date" class="form-control" autocomplete="off">
+    </div>
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Tratamiento <br>dirigido</span>
+      </div>
+      <select name="tratamiento_dirigido" class="tipo form-control">
+        <option {{ $seguimiento->tratamiento_dirigido == 1 ? 'selected' : '' }} value="1">Si</option>
+        <option {{ $seguimiento->tratamiento_dirigido == 0 ? 'selected' : '' }} value="0">No</option>
+      </select>      
+    </div>
+    <div class="d-flex justify-content-center mb-4">
+        <button type="submit" class="btn btn-primary">Modificar</button>
+</form>
+      <form action="{{ route('eliminarseguimiento', ['id' => $paciente->id_paciente, 'num_seguimiento' => $posicion]) }}" method="post">
+        @CSRF
+        @method('delete')
+        <button class="ml-2 btn btn-warning">Eliminar</button>
+      </form>
+    </div>
+<script src="{{ asset('/js/nuevocampo.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/js/especificar_otro.js') }}" type="text/javascript"></script>
+@endsection
---
+++ Laravel/resources/views/seguimientosnuevos.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+ 
+@section('content')
+<div class="d-flex justify-content-between mb-4">
+    <h6 class="align-self-end text-white">Paciente: {{ $paciente->nombre }}</h6>
+    <h1 class="align-self-center text-white panel-title">Nuevo seguimiento</h1>
+    <h6 class="align-self-end text-white">Ultima modificación: {{ $paciente->ultima_modificacion }}</h6>
+</div>
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('fecha')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('fecha_fallecimiento')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@error('motivo_especificar')
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+@if ($message = Session::get('SQLerror'))
+<div class="alert alert-danger alert-block">
+    <button type="button" class="text-dark close" data-dismiss="alert">x</button>
+    <strong class="text-center text-dark">{{ $message }}</strong>
+</div>
+@endif
+<form action="{{ route('crearseguimiento', ['id' => $paciente->id_paciente]) }}" method="post">
+    @CSRF
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Fecha <br>seguimiento</span>
+      </div>
+      <input name="fecha" type="date" class="form-control" autocomplete="off">
+    </div>
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Estado actual <br> del paciente</span>
+      </div>
+      <select name="estado" class="tipoNuevo form-control">
+        <option>Vivo sin enfermedad</option>
+        <option>Vivo con enfermedad</option>
+        <option>Fallecido</option>
+      </select>
+    </div>
+    <div class="oculto ml-2 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Motivo del <br>fallecimiento</span>
+      </div>
+      <select name="motivo" class="tipo form-control">
+        <option>Enfermedad</option>
+        <option>Otro</option>
+      </select>    
+    </div>
+    <div class="oculto ml-4 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Especificar <br>fallecimiento</span>
+      </div>
+      <input name="motivo_especificar" class="form-control" autocomplete="off">
+    </div>
+    <div class="oculto ml-2 my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Fecha del <br> fallecimiento</span>
+      </div>
+      <input name="fecha_fallecimiento" type="date" class="form-control" autocomplete="off">
+    </div>
+    <div class="my-4 input-group">
+      <div class="input-group-prepend">
+          <span class="input-group-text">Tratamiento <br>dirigido</span>
+      </div>
+      <select name="tratamiento_dirigido" class="tipo form-control">
+        <option value="1">Si</option>
+        <option value="0">No</option>
+      </select>      
+    </div>
+    <div class="d-flex justify-content-center mb-4">
+        <button type="submit" class="btn btn-primary">Crear seguimiento</button>
+    </div>
+</form>
+<script src="{{ asset('/js/nuevocampo.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/js/especificar_otro.js') }}" type="text/javascript"></script>
+@endsection
