cd3b1877e1f57ba2e38da1fd7e9ea3918952fbbe
CarlosOrtu <cor1001@alu.ubu.es>
2021-05-31 03:18:27 +0200

    Mejorar estéticamente la web

--- Laravel/resources/views/eliminarpaciente.blade.php
+++ Laravel/resources/views/eliminarpaciente.blade.php
@@ -54,7 +54,10 @@
 	                        <td class="table-text text-dark"><div>{{ $paciente->apellidos }}</div></td>
 	                        @endif
 							<td class="right">
-								<a onClick="confirmarEliminacion(this)"><input type="button" class="btn btn-warning" value="Eliminar"/></a>
+								<a onClick="confirmarEliminacion(this)"><button type="button" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2 bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>Eliminar</button></a>
 							</td>
 	                    </tr>
 	                @endforeach
@@ -80,7 +83,7 @@
 		}
 		var NHC = $(element).parent().prev().text();
 		Swal.fire({
-		  title: 'Estas seguro que deseas eliminar el paciente con el NHC '+NHC,
+		  title: 'Estas seguro que deseas eliminar el paciente con el nombre '+NHC,
 		  icon: "warning",
 		  showDenyButton: true,
 		  confirmButtonText: 'Eliminar',
