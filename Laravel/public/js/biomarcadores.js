ef0b09d439e542b6a1786b90115f3dae965fe6fd
CarlosOrtu <56863397+CarlosOrtu@users.noreply.github.com>
2021-06-15 18:24:33 +0200

    Refactorizar código

--- Laravel/public/js/biomarcadores.js
+++ Laravel/public/js/biomarcadores.js
@@ -1,58 +1,58 @@
   $(document).ready(function(){
     //Función que devuelve los inputs en cuanto se da click a un checkbox
-    $('.casilla').change(function() {
+    $(".casilla").change(function() {
       let id = "#" + this.id;
-      let inputs = $(id).attr('num_input');
-      if($(id+'_tipo_input').val() == "Otra"){
-        $(id+'_tipo_input_especificar').css('display','flex');
-      }else if($(id+'_subtipo_input').val() == "Otra"){
-        $(id+'_subtipo_input_especificar').css('display','flex');
+      let inputs = $(id).attr("num_input");
+      if($(id+"_tipo_input").val() === "Otra"){
+        $(id+"_tipo_input_especificar").css("display","flex");
+      }else if($(id+"_subtipo_input").val() === "Otra"){
+        $(id+"_subtipo_input_especificar").css("display","flex");
       }
-      if(inputs == 2){
-        if($(id).prop('checked')){
-           $(id+'_subtipo').css('display','flex');
-           $(id+'_tipo').css('display','flex');
+      if(inputs === 2){
+        if($(id).prop("checked")){
+           $(id+"_subtipo").css("display","flex");
+           $(id+"_tipo").css("display","flex");
         }else{
-           $(id+'_subtipo').css('display','none');
-           $(id+'_tipo').css('display','none');
-           $(id+'_tipo_input_especificar').css('display','none');
-           $(id+'_subtipo_input_especificar').css('display','none');
+           $(id+"_subtipo").css("display","none");
+           $(id+"_tipo").css("display","none");
+           $(id+"_tipo_input_especificar").css("display","none");
+           $(id+"_subtipo_input_especificar").css("display","none");
         }
       }else{
-        if($(id).prop('checked')){
-           $(id+'_tipo').css('display','flex');
-           if($(id+'_tipo_input').attr('otro_imput') == $(id+'_tipo_input').val()){
-            $(id+'_subtipo').css('display','flex');
+        if($(id).prop("checked")){
+           $(id+"_tipo").css("display","flex");
+           if($(id+"_tipo_input").attr("otro_imput") === $(id+"_tipo_input").val()){
+            $(id+"_subtipo").css("display","flex");
            }
         }else{
-           $(id+'_tipo').css('display','none');
-           $(id+'_tipo_input_especificar').css('display','none');
-           $(id+'_subtipo_input_especificar').css('display','none');
-           if($(id+'_tipo_input').attr('subtipo') == this.id+"_subtipo"){
-            $(id+'_subtipo').css('display','none');
+           $(id+"_tipo").css("display","none");
+           $(id+"_tipo_input_especificar").css("display","none");
+           $(id+"_subtipo_input_especificar").css("display","none");
+           if($(id+"_tipo_input").attr("subtipo") === this.id+"_subtipo"){
+            $(id+"_subtipo").css("display","none");
            }
         }
       }
     });
     //Función que comprueba si un input es de un tipo especifico para devolver el subtipo
-    $('.input_cambio').change(function() {
+    $(".input_cambio").change(function() {
       let id = "#" + this.id;
       let valorActual = $(id).val();
-      let valorActivar = $(id).attr('otro_imput');
-      let inputActivar = $(id).attr('subtipo');
-      if(valorActual == "Otra"){
-        $(id+'_especificar').css('display','flex');
+      let valorActivar = $(id).attr("otro_imput");
+      let inputActivar = $(id).attr("subtipo");
+      if(valorActual === "Otra"){
+        $(id+"_especificar").css("display","flex");
       }else{
-        $(id+'_especificar').css('display','none');
+        $(id+"_especificar").css("display","none");
       }
-      if(valorActual == valorActivar){
-        $('#'+inputActivar).css('display','flex');
-        if($('#'+inputActivar+'_input').val() == "Otra"){
-          $('#'+inputActivar+'_input_especificar').css('display','flex');
+      if(valorActual === valorActivar){
+        $("#"+inputActivar).css("display","flex");
+        if($("#"+inputActivar+"_input").val() === "Otra"){
+          $("#"+inputActivar+"_input_especificar").css("display","flex");
         }
       }else{
-        $('#'+inputActivar).css('display','none');
-        $('#'+inputActivar+'_input_especificar').css('display','none');
+        $("#"+inputActivar).css("display","none");
+        $("#"+inputActivar+"_input_especificar").css("display","none");
       }
     });
   });
--- Laravel/public/js/nuevocampo.js
+++ Laravel/public/js/nuevocampo.js
@@ -3,9 +3,9 @@
     $( "#boton_nuevocampo" ).show();
     $( "#boton_nuevocampo" ).on("click",mostrar);
     function mostrar(){
-      $('#nuevocampo').removeClass("oculto");
-      $('#nuevocampo').show();
-      $("main").animate({ scrollTop: $('#nuevocampo').offset().top },200);
+      $("#nuevocampo").removeClass("oculto");
+      $("#nuevocampo").show();
+      $("main").animate({ scrollTop: $("#nuevocampo").offset().top },200);
       $( "#boton_nuevocampo" ).hide();
     } 
 });
